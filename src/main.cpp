380bda0d07b83951cd98fc1204092b06080f90b6
Dehao Dai <dai.de.hao@163.com>
2019-12-14 21:14:49 -0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,31 @@
 #include <fstream>
 #include "../header/multiple_commands.h"
 
-int main(int argv,char** argc){
-	Multiple_Commands* mc=new Multiple_Commands(argc[1]);
-	mc->do_commands();
+int main(){
+	std::string st="";
+	while (true) {
+		//dup2(0,0);
+		//dup2(0,0);
+		//for (int i=3;i<10;i++) close(i);i
+		int saved_stdin = dup(STDIN_FILENO);
+		int saved_stdout = dup(STDOUT_FILENO);
+		std::getline(std::cin,st);
+		if (st=="exit") break;
+		else {
+			st+="\n";
+		      	std::ofstream outfile("infile.txt");
+			outfile<<st;	
+			outfile.close();
+			Multiple_Commands* mc=new Multiple_Commands("infile.txt");
+		//std::cout<<"pass\n";
+			mc->do_commands();
+		//dup2(0,0);
+		//dup2(1,1);
+		}
+		dup2(saved_stdin, STDIN_FILENO);
+		close(saved_stdin);
+		dup2(saved_stdout, STDOUT_FILENO);
+		close(saved_stdout);
+	}
 }
 
--- unit_tests/precedence_tests.hpp
+++ unit_tests/precedence_tests.hpp
@@ -1,10 +1,11 @@
-#ifndef __PRECEDENCE_TESTS_HPP__
-#define __PRECEDENCE_TESTS_HPP__
+#ifndef __TEST_LITERAL_TESTS_HPP__
+#define __TEST_LITERAL_TESTS_HPP__
 #include "../header/base_commands.h"	
 #include "../header/commented_commands.h" 
 #include "../header/multiple_commands.h" 
 #include "../header/single_commands.h"
-TEST(Precedence_tests, TEST2) {	
+#include "../header/test_commands.h"
+TEST(Test_Literal_Tests, TEST2) {	
 	char* st="integration_tests/precedence_tests.txt";
 	Multiple_Commands* mc=new Multiple_Commands(st);
 	EXPECT_TRUE(mc->do_commands());
